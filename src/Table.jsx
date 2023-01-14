2bc53f68931a2d68ef28e4d872cd7445f14e8667
amermarei <amermarei730@yahoo.com>
2023-01-15 00:28:46 +0200

    initial commit

--- src/Table.jsx
+++ src/Table.jsx
@@ -5,13 +5,10 @@ import axios from "axios";
 import { Fragment } from "react";
 function Table() {
     let [users, setUsers] = useState([]);
-    let [isLoading, setIsLoading] = useState(false);
     const fetchAPI = async () => {
-        setIsLoading(true);
         let { data } = await axios.get(
             `https://random-data-api.com/api/v2/users?size=50&is_xml=true`
         );
-        setIsLoading(false);
         setUsers(data);
     };
     useEffect(() => {
