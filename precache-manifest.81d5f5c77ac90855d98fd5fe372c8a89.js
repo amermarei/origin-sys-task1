d71686cddca1bd0569c81c6b6881f3ab167b25d0
amermarei <amermarei730@yahoo.com>
2023-01-14 22:15:48 +0200

    Updates

---
+++ precache-manifest.81d5f5c77ac90855d98fd5fe372c8a89.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00879ec557ea9162dab71ef874c28f5",
+    "url": "/origin-sys-task1/index.html"
+  },
+  {
+    "revision": "73174c579737e1793481",
+    "url": "/origin-sys-task1/static/css/main.e3e039b7.chunk.css"
+  },
+  {
+    "revision": "7311a9af4e33aeaa0141",
+    "url": "/origin-sys-task1/static/js/2.cff49144.chunk.js"
+  },
+  {
+    "revision": "8d248d20ba8d7878158019721e41af93",
+    "url": "/origin-sys-task1/static/js/2.cff49144.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b574e95fc5dd83ae6b",
+    "url": "/origin-sys-task1/static/js/3.a85c43c2.chunk.js"
+  },
+  {
+    "revision": "6e31a0db202a37e424180fec8d0f2d9a",
+    "url": "/origin-sys-task1/static/js/3.a85c43c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af81877202ab8c3784d8",
+    "url": "/origin-sys-task1/static/js/4.09d87d75.chunk.js"
+  },
+  {
+    "revision": "d61ae33b47dd0dede9f5ae3c241a7eca",
+    "url": "/origin-sys-task1/static/js/4.09d87d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57a327678df473d796d3",
+    "url": "/origin-sys-task1/static/js/5.802e8eff.chunk.js"
+  },
+  {
+    "revision": "4c2f580656954a1ad1c84a3bcb195d1c",
+    "url": "/origin-sys-task1/static/js/5.802e8eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73174c579737e1793481",
+    "url": "/origin-sys-task1/static/js/main.711a7ff0.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/origin-sys-task1/static/js/main.711a7ff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9399792edfd554315fdf",
+    "url": "/origin-sys-task1/static/js/runtime-main.889c3242.js"
+  }
+]);
